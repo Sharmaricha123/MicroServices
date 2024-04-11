4a55edcf4c6ad9945f3534cb7203f4b62c141ed6
Sharmaricha123 <sharmarichaprof2017@gmail.com>
2024-04-11 13:23:58 +0530

    feign client to implement post http call

--- User-Service/src/main/java/com/user/external/services/RatingService.java
+++ User-Service/src/main/java/com/user/external/services/RatingService.java
@@ -1,8 +1,25 @@
 package com.user.external.services;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import com.user.entities.Rating;
 
 @FeignClient(name="RATING-SERVICE")
 public interface RatingService {
+	
+	@PostMapping("/ratings")
+	public ResponseEntity<Rating> create(Rating rating);
+	
+	
+	@PutMapping("/rating/{ratingId}")
+	public ResponseEntity<Rating> updareRating(@PathVariable String ratingId,Rating rating);
+	
+	@DeleteMapping("/ratings/{ratingId}")
+	public void deleteRating(@PathVariable String ratingId);
 
 }
--- User-Service/src/test/java/com/user/UserServiceApplicationTests.java
+++ User-Service/src/test/java/com/user/UserServiceApplicationTests.java
@@ -1,7 +1,12 @@
 package com.user;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import com.user.entities.Rating;
+import com.user.external.services.RatingService;
 
 @SpringBootTest
 class UserServiceApplicationTests {
@@ -9,5 +14,17 @@ class UserServiceApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	
+	@Autowired
+	private RatingService ratingService;
+
+	@Test
+	void createRating() {
+		
+		Rating rating=Rating.builder().rating(10).userId("").hotelId("").feedback("Thos hotel is environment friendly").build();
+		ResponseEntity<Rating> savedRating=ratingService.create(rating);
+		System.out.println("new rating created");
+	}
+	
 
 }
